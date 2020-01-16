0fd7e56080f6ede9ef4de37fcdd1d847206ea05b
mridentity <mridentity>
2020-01-16 13:38:29 -0500

    Add ReadyConnect external authentication method.

---
+++ src/Plugins/Nop.Plugin.ExternalAuth.ReadyConnect/ReadyConnectAuthenticationMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Authentication.External;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.ExternalAuth.ReadyConnect
+{
+    public class ReadyConnectAuthenticationMethod : BasePlugin, IExternalAuthenticationMethod
+    {
+        public string GetPublicViewComponentName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
